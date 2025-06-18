0665ae707182425441d30e479807d1840bfce9d1
Lakshay Kumar.R.B <lakshaikumar11@gmail.com>
2025-06-18 22:06:57 +0530

    Initial commit: Factlink blog setup

---
+++ data/headerNavLinks.ts
@@ -0,0 +1,9 @@
+const headerNavLinks = [
+  { href: '/', title: 'Home' },
+  { href: '/blog', title: 'Articles' },
+  { href: '/tags', title: 'Topics' },
+  { href: '/projects', title: 'Projects' },
+  { href: '/about', title: 'About Factlink' },
+]
+
+export default headerNavLinks
