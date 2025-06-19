5f14b334ffeeb2b5de3addb3949dd64a1f8b8c15
Lakshay Kumar.R.B <lakshaikumar11@gmail.com>
2025-06-19 21:28:47 +0530

    easy

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,17 +2,15 @@ import Link from '@/components/Link'
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-start justify-start md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6">
-      <div className="space-x-2 pt-6 pb-8 md:space-y-5">
-        <h1 className="text-6xl leading-9 font-extrabold tracking-tight text-gray-900 md:border-r-2 md:px-6 md:text-8xl md:leading-14 dark:text-gray-100">
-          404
-        </h1>
-      </div>
-      <div className="max-w-md">
-        <p className="mb-4 text-xl leading-normal font-bold md:text-2xl">
-          Sorry we couldn't find this page.
+    <div className="min-h-screen w-full flex items-center justify-center transition-colors duration-500">
+      <div className="flex flex-col items-center max-w-lg w-full">
+        <h1 className="text-7xl md:text-9xl font-extrabold text-gray-900 dark:text-gray-100 mb-4">404</h1>
+        <p className="mb-2 text-2xl font-bold text-gray-900 dark:text-gray-100 text-center">
+          Sorry, we couldn't find this page.
         </p>
-        <p className="mb-8">But dont worry, you can find plenty of other things on our homepage.</p>
+        <p className="mb-8 text-center text-gray-700 dark:text-gray-300">
+          But don't worry, you can find plenty of other things on our homepage.
+        </p>
         <Link
           href="/"
           className="focus:shadow-outline-blue inline rounded-lg border border-transparent bg-blue-600 px-4 py-2 text-sm leading-5 font-medium text-white shadow-xs transition-colors duration-150 hover:bg-blue-700 focus:outline-hidden dark:hover:bg-blue-500"
